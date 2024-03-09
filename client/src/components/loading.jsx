20b0dcd453691ce80c23deb5a0cbc0b84d3012d4
alpturan-dev <abdurrahmanalpturan13@hotmail.com>
2024-03-10 00:15:08 +0300

    fix loading

--- client/src/components/loading.jsx
+++ client/src/components/loading.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export function Loading({ children, loading }) {
+export default function Loading({ children, loading }) {
     return (
         <>{!loading ? <>{children}</> :
             <div className="flex justify-center items-center h-screen fixed top-0 left-0 right-0 bottom-0 w-full z-50 overflow-hidden bg-gray-300 opacity-75">
