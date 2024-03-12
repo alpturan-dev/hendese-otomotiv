f5b358e08624434c70d8d50e34bbe3369dc50707
alpturan-dev <abdurrahmanalpturan13@hotmail.com>
2024-03-12 16:24:57 +0300

    add categories section

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -25,8 +25,8 @@ const Navbar = () => {
                     <img src={logo} className='w-[8rem] md:w-[10rem] h-[5rem] md:h-[6rem] mx-auto md:mx-0' />
                 </div>
                 <div className="w-full md:w-2/4 flex flex-col gap-4">
-                    <div >
-                        <div className='text-base md:text-xl lg:text-2xl font-bold text-center'>Hendese Otomotiv</div>
+                    <div>
+                        <div className='text-xl md:text-2xl lg:text-3xl font-bold text-center mb-1'>Hendese Otomotiv</div>
                         <div className='text-base md:text-xl lg:text-2xl font-bold text-center underline underline-offset-4 decoration-[#E3020F]'><span className='text-[#E3020F]'>Suzuki</span> Çıkma Yedek Parça</div>
                     </div>
                     <div className='relative h-10 w-4/5 mx-auto flex items-center gap-2 mb-6 md:mb-0'>
--- client/src/pages/dashboard/index.jsx
+++ client/src/pages/dashboard/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '@/components/navbar'
 import CustomCarousel from '@/components/custom-carousel'
+import CategoriesSection from './components/categories-section'
 
 const Dashboard = () => {
     return (
         <div>
             <Navbar />
             <CustomCarousel />
+            <CategoriesSection />
         </div>
     )
 }
